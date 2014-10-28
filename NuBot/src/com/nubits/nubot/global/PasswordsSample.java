e29a727e71678a116dca14fd3fdef96b65d48ce9
Desrever Nu <desrever.nu@gmail.com>
2014-10-28 10:50:37 +0100

    - re addedd password sample

---
+++ NuBot/src/com/nubits/nubot/global/PasswordsSample.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 desrever <desrever at nubits.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.nubits.nubot.global;
+
+/**
+ * Important : After filling variables, Rename this class to Passwords.java
+ */
+public class PasswordsSample {
+
+    public static final String NUD_RPC_USER = "xxx";
+    public static final String NUD_RPC_PASS = "xxx";
+    public static final String CUSTODIAN_PUBLIC_ADDRESS = "xxx"; //testnet elected custodian
+    public static final String BTCE_SECRET = "xxx";
+    public static final String BTCE_KEY = "xxx";
+    public static final String PEATIO_SECRET = "xxx";
+    public static final String PEATIO_KEY = "xxx";
+    public static final String CCEDK_SECRET = "xxx";
+    public static final String CCEDK_KEY = "xxx";
+    public static final String BTER_KEY = "xxx";
+    public static final String BTER_SECRET = "xxx";
+    public static final String POLONIEX_SECRET = "xxx";
+    public static final String POLONIEX_KEY = "xxx";
+    public static final String CCEX_SECRET = "xxx"; //
+    public static final String GMAIL_USERNAME = "xxx"; //used to send mail notifications
+    public static final String GMAIL_PASSWORD = "xxx";
+    public static final String HIPCHAT_KEY = "xxx"; //not admin
+    public static final String HIPCHAT_LIQUIDITY_ROOM_ID = "xxx"; //Liquidity Testing
+    public static final String OPEN_EXCHANGE_RATES_APP_ID = "xxx";
+    public static final String EXCHANGE_RATE_LAB = "xxx";
+}
