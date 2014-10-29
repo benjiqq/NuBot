db0ea2b440dd79caa492329bad009f1f20a9810a
MJ2P <mj2p.sg@gmail.com>
2014-10-29 22:23:47 +0000

    moved Passwords to PasswordsSample in line with other branches

--- NuBot/src/com/nubits/nubot/global/PasswordsSample.java
+++ NuBot/src/com/nubits/nubot/global/PasswordsSample.java
@@ -20,7 +20,7 @@ package com.nubits.nubot.global;
 /**
  * Important : After filling variables, Rename this class to Passwords.java
  */
-public class Passwords {
+public class PasswordsSample {
 
     public static final String NUD_RPC_USER = "xxx";
     public static final String NUD_RPC_PASS = "xxx";
