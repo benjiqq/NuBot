572eedfa2cdea75fca07d4571c1b68f128784c34
Desrever Nu <desrever.nu@gmail.com>
2014-10-31 15:22:48 +0100

    - Testing hipchat notification made easier.

--- NuBot/src/com/nubits/nubot/notifications/HipChatNotifications.java
+++ NuBot/src/com/nubits/nubot/notifications/HipChatNotifications.java
@@ -39,11 +39,12 @@ public class HipChatNotifications {
 
     public static void sendMessage(String message, Color color) {
         String publicAddress = "";
-        boolean send = false; // default don't send
-        boolean notify = false; //default don't notify
+        boolean send = true; // default send
+        boolean notify = true; //default notify
         if (Global.options != null) {
             publicAddress = Global.options.getNubitsAddress();
             send = Global.options.isSendHipchat();
+            notify = false;
         }
 
         String toSend = message + " (" + publicAddress + ")";
