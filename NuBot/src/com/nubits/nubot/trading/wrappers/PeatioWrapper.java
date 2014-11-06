2250d09096b8b05bd2b1ae5cae9a6a44ba37220c
MJ2P <mj2p.sg@gmail.com>
2014-11-06 20:12:53 +0000

    fix for issue #1. Too many characters were being removed from the ned of the supplied time stamp. Figures were removed to remove the time zone. The returned time stamp didn't match the example provided in the comments.

--- NuBot/src/com/nubits/nubot/trading/wrappers/PeatioWrapper.java
+++ NuBot/src/com/nubits/nubot/trading/wrappers/PeatioWrapper.java
@@ -386,7 +386,7 @@ public class PeatioWrapper implements TradeInterface {
 
         /*Sample result
          */
-        LOG.info(queryResult);
+
 
         JSONArray response = JSONObject.parseArray(queryResult);
         for (Object anOrdersResponse : response) {
@@ -600,7 +600,6 @@ public class PeatioWrapper implements TradeInterface {
             }
         }
 
-
         //Create a CurrencyPair object
         CurrencyPair cp = CurrencyPair.getCurrencyPairFromString(jsonObject.getString("market"), "");
 
@@ -610,9 +609,7 @@ public class PeatioWrapper implements TradeInterface {
         order.setAmount(new Amount(jsonObject.getDouble("remaining_volume"), cp.getOrderCurrency()));
         order.setPrice(new Amount(jsonObject.getDouble("price"), cp.getPaymentCurrency()));
 
-        LOG.info("Created Date = " + parseDate(jsonObject.getString("created_at")));
         order.setInsertedDate(parseDate(jsonObject.getString("created_at")));
-
         order.setType(jsonObject.getString("side"));
         //Created at?
 
@@ -634,10 +631,10 @@ public class PeatioWrapper implements TradeInterface {
     private Date parseDate(String dateStr) {
         Date toRet = null;
         //Parse the date
-        //Sample 2014-08-19T10:23:49+01:00
+        //Sample 2014-08-19T10:23:49Z
 
         //Remove the Timezone
-        dateStr = dateStr.substring(0, dateStr.length() - 6);
+        dateStr = dateStr.substring(0, dateStr.length() - 1);
         String datePattern = "yyyy-MM-dd'T'HH:mm:ss";
         DateFormat df = new SimpleDateFormat(datePattern, Locale.ENGLISH);
         try {
