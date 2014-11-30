ae9d130787bb99dc267c8d9fed331bd633e10816
MJ2P <mj2p.sg@gmail.com>
2014-11-30 20:55:56 +0000

    updates to Service wrapper

--- NuBot/src/com/nubits/nubot/trading/wrappers/ExcoinWrapper.java
+++ NuBot/src/com/nubits/nubot/trading/wrappers/ExcoinWrapper.java
@@ -1,6 +1,7 @@
 package com.nubits.nubot.trading.wrappers;
 
 import com.nubits.nubot.exchanges.Exchange;
+import com.nubits.nubot.global.Global;
 import com.nubits.nubot.models.ApiError;
 import com.nubits.nubot.models.ApiResponse;
 import com.nubits.nubot.models.Currency;
@@ -36,11 +37,36 @@ public class ExcoinWrapper implements TradeInterface{
     private final String SIGN_HASH_FUNCTION = "HmacSHA256";
     private final String ENCODING = "UTF-8";
     private final int EXPIRE_TIMESTAMP = 900; //15 minutes
+    private String apiBaseUrl;
+    private String checkConnectionUrl;
     //API Paths
     private final String API_BASE_URL = "https://api.exco.in/v1/account";
     private final String API_SUMMARY = "summary";
     private final String API_TRADES = "trades";
     private final String API_ORDERS = "orders";
+    private final String API_TRADE = "issue";
+    private final String API_ORDER = "order";
+    private final String API_CANCEL = "cancel";
+    //Errors
+    private ErrorManager errors = new ErrorManager();
+    private final String TOKEN_ERR = "error";
+    private final String TOKEN_BAD_RETURN = "No Connection With Exchange";
+
+    public ExcoinWrapper() {
+        setupErrors();
+    }
+
+    public ExcoinWrapper(ApiKeys keys, Exchange exchange, String api_base) {
+        this.keys = keys;
+        this.exchange = exchange;
+        this.apiBaseUrl = api_base;
+        this.checkConnectionUrl = api_base;
+        setupErrors();
+    }
+
+    private void setupErrors() {
+        errors.setExchangeName(exchange);
+    }
 
 
     @Override
@@ -130,7 +156,15 @@ public class ExcoinWrapper implements TradeInterface{
 
     @Override
     public String query(String url, HashMap<String, String> args, boolean isGet) {
-        return null;
+        ExcoinService query = new ExcoinService(url, keys);
+        String queryResult;
+        if (exchange.getLiveData().isConnected()) {
+            queryResult = query.executeQuery(true, isGet);
+        } else {
+            LOG.severe("The bot will not execute the query, there is no connection to Excoin");
+            queryResult = TOKEN_BAD_RETURN;
+        }
+        return queryResult;
     }
 
     @Override
@@ -165,9 +199,103 @@ public class ExcoinWrapper implements TradeInterface{
 
     private class ExcoinService implements ServiceInterface {
 
+        protected String url;
+        protected ApiKeys keys;
+
+        public ExcoinService(String url, ApiKeys keys) {
+            this.url = url;
+            this.keys = keys;
+        }
+
         @Override
         public String executeQuery(boolean needAuth, boolean isGet) {
-            return null;
+            HttpsURLConnection connection = null;
+            boolean httpError = false;
+            String output;
+            int response = 200;
+            String answer = null;
+            URL queryUrl = null;
+            String post_data = "";
+
+            try {
+                queryUrl = new URL(url);
+            } catch (MalformedURLException mal) {
+                LOG.severe(mal.toString());
+            }
+
+
+            try {
+                connection = (HttpsURLConnection) queryUrl.openConnection();
+                connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
+                connection.setRequestProperty("User-Agent", Global.settings.getProperty("app_name"));
+                connection.setRequestProperty("Accept", "*/*");
+                if (needAuth) {
+                    connection.setRequestProperty("Api-Key", keys.getApiKey());
+                    connection.setRequestProperty("Api-Signature", signRequest(keys.getPrivateKey(), url));
+                }
+                connection.setRequestProperty("Connection", "close");
+                connection.setRequestProperty("Host", "exco.in");
+
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+
+                if (isGet) {
+                    connection.setRequestMethod("GET");
+                } else {
+                    connection.setRequestMethod("POST");
+                    DataOutputStream os = new DataOutputStream(connection.getOutputStream());
+                    os.writeBytes(post_data);
+                    os.flush();
+                    os.close();
+                }
+            } catch (ProtocolException pe) {
+                LOG.severe(pe.toString());
+            } catch (IOException io) {
+                LOG.severe((io.toString()));
+            }
+
+
+            BufferedReader br = null;
+            try {
+                if (connection.getResponseCode() >= 400) {
+                    httpError = true;
+                    response = connection.getResponseCode();
+                    br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                } else {
+                    answer = "";
+                    br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                }
+            } catch (IOException io) {
+                LOG.severe(io.toString());
+            }
+
+            if (httpError) {
+                LOG.severe("Query to : " + url +
+                        "\nHTTP Response : " + Objects.toString(response));
+            }
+
+            try {
+                while ((output = br.readLine()) != null) {
+                    answer += output;
+                }
+            } catch (IOException io) {
+                LOG.severe(io.toString());
+            }
+
+            if (httpError) {
+                JSONParser parser = new JSONParser();
+                try {
+                    JSONObject obj = (JSONObject) (parser.parse(answer));
+                    answer = (String) obj.get(TOKEN_ERR);
+                } catch (ParseException pe) {
+                    LOG.severe(pe.toString());
+                }
+            }
+
+            connection.disconnect();
+            connection = null;
+
+            return answer;
         }
 
         @Override
