ba2cabfbcaf2770be3346c00a183ecf7b3a84824
Desrever Nu <desrever.nu@gmail.com>
2015-01-12 16:44:05 +0100

    stress tests

--- NuBot/src/com/nubits/nubot/tests/TestWrappers.java
+++ NuBot/src/com/nubits/nubot/tests/TestWrappers.java
@@ -56,7 +56,7 @@ public class TestWrappers {
         init();
         Global.options = OptionsJSON.parseOptions(TEST_OPTIONS_PATH);
 
-        configExchange(Constant.BTER); //Replace to test a different API implementation
+        configExchange(Constant.CCEDK); //Replace to test a different API implementation
 
         runTests();
         System.exit(0);
@@ -83,8 +83,8 @@ public class TestWrappers {
         //testCancelOrder("2063803", Constant.NBT_BTC);
         //testClearAllOrders(Constant.NBT_BTC);
         //testIsOrderActive("2064456");
-        testGetTxFee();
-        testGetTxFeeWithArgs(Constant.BTC_USD);
+        //testGetTxFee();
+        //testGetTxFeeWithArgs(Constant.BTC_USD);
         //Methods NOT strictly necessary for NuBot to run---------------
         //---------------
         //testGetLastPrice(Constant.NBT_BTC);
@@ -101,39 +101,39 @@ public class TestWrappers {
         //}
         //stimulating ccedk wrong nonce
 /*
-        for (int i = 0; i < 60; i++) {
+         for (int i = 0; i < 60; i++) {
+         try {
+         String htmlString = Utils.getHTML("https://www.ccedk.com/api/v1/currency/list?nonce=1234567891", false);
+         LOG.warning(htmlString);
+         } catch (IOException io) {
+         LOG.severe(io.toString());
+         }
+         }
+         */
+
+        for (int i = 0; i < 5000; i++) {
+            testGetActiveOrders();
             try {
-                String htmlString = Utils.getHTML("https://www.ccedk.com/api/v1/currency/list?nonce=1234567891", false);
-                LOG.warning(htmlString);
-            } catch (IOException io) {
-                LOG.severe(io.toString());
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                LOG.severe(ex.toString());
+            }
+
+            testGetAvailableBalances(Constant.NBT_BTC);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                LOG.severe(ex.toString());
+            }
+            testGetOrderDetail("3454");
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException ex) {
+                LOG.severe(ex.toString());
             }
         }
-*/
-
-         for (int i = 0; i < 5000; i++) {
-             testGetActiveOrders();
-             try {
-                 Thread.sleep(100);
-             } catch (InterruptedException ex) {
-                 LOG.severe(ex.toString());
-             }
-
-             testGetAvailableBalances(Constant.NBT_BTC);
-
-             try {
-                 Thread.sleep(100);
-             } catch (InterruptedException ex) {
-                 LOG.severe(ex.toString());
-             }
-             testGetOrderDetail("3454");
-
-             try {
-                 Thread.sleep(300);
-             } catch (InterruptedException ex) {
-                 LOG.severe(ex.toString());
-             }
-         }
 
     }
 
