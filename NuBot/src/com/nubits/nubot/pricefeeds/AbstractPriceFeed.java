f9cd4eb635bc669ca6ac94ea72ba57a2d0e00678
MJ2P <mj2p.sg@gmail.com>
2015-01-29 21:26:56 +0000

    updated copyright details to Nu Development Team

--- NuBot/src/com/nubits/nubot/pricefeeds/AbstractPriceFeed.java
+++ NuBot/src/com/nubits/nubot/pricefeeds/AbstractPriceFeed.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2014 desrever <desrever at nubits.com>
+ * Copyright (C) 2014 Nu Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
