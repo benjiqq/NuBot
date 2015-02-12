827b2e5905804d652e349aa42815a7164a9ff619
Desrever Nu <desrever.nu@gmail.com>
2015-02-12 12:03:24 +0100

    #200 add changelog.html to root

--- compileReadme.sh
+++ compileReadme.sh
@@ -2,3 +2,4 @@
 /usr/local/bin/grip README.md --export readme.html
 /usr/local/bin/grip SETUP.md --export setup.html
 /usr/local/bin/grip FEEDS.md --export feeds.html
+/usr/local/bin/grip CHANGELOG.md --export changelog.html
