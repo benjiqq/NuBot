ede5dad4aabe023f52d8fe1c5cf04cd83e5eeaad
benjyz <benjamin.l.cordes@gmail.com>
2015-02-12 11:31:38 +0100

    working index

--- NuBot/src/com/nubits/nubot/webui/UIServer.java
+++ NuBot/src/com/nubits/nubot/webui/UIServer.java
@@ -1,7 +1,19 @@
 package com.nubits.nubot.webui;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  * basic server for setting up a WebUI
@@ -12,6 +24,20 @@ public class UIServer {
     private static final int TESTNET_UI_PORT = 2875;
 
     private static Server server;
+
+    public static class HelloServlet extends HttpServlet {
+        private static final String greeting = "Hello World";
+
+        protected void doGet(HttpServletRequest request,
+                HttpServletResponse response) throws ServletException,
+                IOException {
+
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println(greeting);
+        }
+
+    }
 
     public static void main(String[] args) {
 
@@ -24,42 +50,41 @@ public class UIServer {
         ServerConnector connector = new ServerConnector(server);
         connector.setPort(port);
         connector.setHost(host);
-        
+
+        // connector.setIdleTimeout(getIntProperty("uiServerIdleTimeout"));
+        // connector.setReuseAddress(true);
+
         server.addConnector(connector);
-        
+
+        HandlerList userHandlers = new HandlerList();
+
+        ResourceHandler userFileHandler = new ResourceHandler();
+        userFileHandler.setDirectoriesListed(false);
+        userFileHandler.setWelcomeFiles(new String[] { "index.html" });
+        // userFileHandler.setResourceBase(getStringProperty("uiResourceBase"));
+        userFileHandler.setResourceBase("html");
+
+        userHandlers.addHandler(userFileHandler);
+
+        ServletHandler userHandler = new ServletHandler();
+        // ServletHolder userHolder =
+        // userHandler.addServletWithMapping(UserServlet.class, "/nu");
+        ServletHolder u = userHandler.addServletWithMapping(HelloServlet.class,
+                "/");
+        // u.setAsyncSupported(true);
+
+        userHandlers.addHandler(userHandler);
+
+        userHandlers.addHandler(new DefaultHandler());
+
+        server.setHandler(userHandlers);
+
         try {
             server.start();
             server.join();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        
-        // connector.setIdleTimeout(Nxt.getIntProperty("nxt.uiServerIdleTimeout"));
-        // connector.setReuseAddress(true);
-        // userServer.addConnector(connector);
-        //
-        //
-        // HandlerList userHandlers = new HandlerList();
-        //
-        // ResourceHandler userFileHandler = new ResourceHandler();
-        // userFileHandler.setDirectoriesListed(false);
-        // userFileHandler.setWelcomeFiles(new String[]{"index.html"});
-        // userFileHandler.setResourceBase(Nxt.getStringProperty("nxt.uiResourceBase"));
-        //
-        // userHandlers.addHandler(userFileHandler);
-        //
-        // ServletHandler userHandler = new ServletHandler();
-        // ServletHolder userHolder =
-        // userHandler.addServletWithMapping(UserServlet.class, "/nxt");
-        // userHolder.setAsyncSupported(true);
-        //
-        //
-        // userHandlers.addHandler(userHandler);
-        //
-        // userHandlers.addHandler(new DefaultHandler());
-        //
-        // userServer.setHandler(userHandlers);
         // userServer.setStopAtShutdown(true);
         //
         //
