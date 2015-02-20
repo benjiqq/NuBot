9607171b742e18f8bf49578a062290e0c6c38156
benjyz <benjamin.l.cordes@gmail.com>
2015-02-20 20:44:04 +0100

    put api key

--- NuBot/src/com/nubits/nubot/webui/UiServer.java
+++ NuBot/src/com/nubits/nubot/webui/UiServer.java
@@ -1,19 +1,26 @@
 package com.nubits.nubot.webui;
 
+import com.nubits.nubot.options.NuBotOptions;
+import com.nubits.nubot.options.ParseOptions;
+import com.nubits.nubot.options.SaveOptions;
 import com.nubits.nubot.webui.service.StockServiceServer;
 import spark.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.*;
- 
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class UiServer {
 
     //TODO path
     private static String htmlFolder = "./html/tmpl/";
 
-    public static void startService(){
+    private static String testconfigFile = "test.json";
+    private static String testconfig = "testconfig/" + testconfigFile;
+
+    public static void startService() {
         try {
             new StockServiceServer().run();
         } catch (Exception e) {
@@ -23,6 +30,7 @@ public class UiServer {
 
     /**
      * start the UI
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -41,7 +49,27 @@ public class UiServer {
         }, new JsonTransformer());
 
         post("/keys", "application/json", (request, response) -> {
-            return "try put: " + request.body();
+            String resp = request.body();
+
+            //Map<String, String> rmap = request.params();
+            //Iterator<String> it = rmap.keySet().iterator();
+            //while (it.hasNext()) {
+            //    String c = it.next();
+            //    resp += c;
+            //
+            // }
+            String apikey = request.queryParams("apikey");
+
+
+            //SaveOptions.backupOptions(testconfig);
+            //NuBotOptions opt = ParseOptions.parseOptionsSingle(testconfig);
+            //opt.setApiKey(apikey);
+            //SaveOptions.saveOptions(opt,testconfig);
+
+            //TODO: return success
+
+
+            return "try put: " + apikey;
         });
 
     }
