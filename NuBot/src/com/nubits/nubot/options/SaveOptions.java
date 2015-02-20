ac9ea37a20f331de4551a1d8950531b1b7dd5a2d
benjyz <benjamin.l.cordes@gmail.com>
2015-02-20 16:10:48 +0100

    save options to file

---
+++ NuBot/src/com/nubits/nubot/options/SaveOptions.java
@@ -0,0 +1,16 @@
+package com.nubits.nubot.options;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.nubits.nubot.utils.FileSystem;
+
+public class SaveOptions {
+
+    public static boolean saveOptions(NuBotOptions opt, String filepath) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String jsonOpt = gson.toJson(opt);
+        FileSystem.writeToFile(jsonOpt, filepath, false);
+    }
+}
--- NuBot/src/com/nubits/nubot/tests/TestWriteOptions.java
+++ NuBot/src/com/nubits/nubot/tests/TestWriteOptions.java
@@ -3,13 +3,16 @@ package com.nubits.nubot.tests;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.nubits.nubot.options.NuBotOptions;
+import com.nubits.nubot.options.SaveOptions;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.io.File;
+
 public class TestWriteOptions extends TestCase {
 
-    private static String testconfigFile = "test.json";
-    private static String testconfig = "testconfig/" + testconfigFile;
+    private static String testOutconfigFile = "testout.json";
+    private static String testconfig = "testconfig/" + testOutconfigFile;
 
     @Override
     public void setUp() {
@@ -23,10 +26,21 @@ public class TestWriteOptions extends TestCase {
 
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        String j =gson.toJson(opt);
-        System.out.println(j);
-        assertTrue(j.startsWith("{"));
-        assertTrue(j.endsWith("}"));
+        String jsonOpt =gson.toJson(opt);
+        //System.out.println(jsonOpt);
+        assertTrue(jsonOpt.startsWith("{"));
+        assertTrue(jsonOpt.endsWith("}"));
+        assertTrue(jsonOpt.contains("verbose"));
+        assertTrue(jsonOpt.contains("dualSide\":false"));
+    }
+
+    @Test
+    public void testSave() {
+        NuBotOptions opt = new NuBotOptions();
+
+        SaveOptions.saveOptions(opt,testOutconfigFile);
+        File f = new File(testOutconfigFile);
+        assertTrue(f.exists());
     }
 
 
