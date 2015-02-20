f7429fd1b727b4ce35e9a8edfbe25680481a0a08
benjyz <benjamin.l.cordes@gmail.com>
2015-02-20 13:07:36 +0100

    json from UI. remove websockets

--- NuBot/src/com/nubits/nubot/webui/UiServer.java
+++ NuBot/src/com/nubits/nubot/webui/UiServer.java
@@ -8,23 +8,39 @@ import java.util.Map;
 import spark.ModelAndView;
  
 public class UiServer {
-    
- 
-    public static void main(String[] args) {
-        
+
+    //TODO path
+    private static String htmlFolder = "./html/tmpl/";
+
+    public static void startService(){
         try {
             new StockServiceServer().run();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * start the UI
+     * @param args
+     */
+    public static void main(String[] args) {
         
+
         Map map = new HashMap();
         map.put("name", "Yo");
 
-        String htmlFolder = ""
 
-        get("/", (rq, rs) -> new ModelAndView(map, "./html/tmpl/config.mustache"), new LayoutTemplateEngine());
+        get("/", (rq, rs) -> new ModelAndView(map, htmlFolder + "config.mustache"), new LayoutTemplateEngine());
 
-        get("/log", (rq, rs) -> new ModelAndView(map, "./html/tmpl/log.mustache"), new LayoutTemplateEngine());
+        get("/log", (rq, rs) -> new ModelAndView(map, htmlFolder + "log.mustache"), new LayoutTemplateEngine());
+
+
+        Msg keyMsg = new Msg("key");
+
+        get("/hello", "application/json", (request, response) -> {
+            return keyMsg;
+        }, new JsonTransformer());
+
     }
 }
