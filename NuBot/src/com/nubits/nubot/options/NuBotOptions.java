ef9d94cd24a79b4168ad58eca150331f9e6ad3be
benjyz <benjamin.l.cordes@gmail.com>
2015-02-27 11:50:09 +0100

    fix old secondary stuff

--- NuBot/src/com/nubits/nubot/options/NuBotOptions.java
+++ NuBot/src/com/nubits/nubot/options/NuBotOptions.java
@@ -61,11 +61,6 @@ public class NuBotOptions {
      */
     public CurrencyPair pair;
 
-    /**
-     * options for PEG other than NBT/USD
-     */
-    public SecondaryPegOptionsJSON secondaryPegOptions;
-
     //Conditional settings with a default value
 
     /**
@@ -567,20 +562,6 @@ public class NuBotOptions {
         this.mailRecipient = mailRecipient;
     }
 
-    /**
-     * @return
-     */
-    public SecondaryPegOptionsJSON getSecondaryPegOptions() {
-        return secondaryPegOptions;
-    }
-
-    /**
-     * @param secondaryPegOptions
-     */
-    public void setCryptoPegOptions(SecondaryPegOptionsJSON secondaryPegOptions) {
-        this.secondaryPegOptions = secondaryPegOptions;
-    }
-
     public boolean isMultipleCustodians() {
         return multipleCustodians;
     }
@@ -703,11 +684,7 @@ public class NuBotOptions {
 
     @Override
     public String toString() {
-        String cryptoOptions = "";
-        if (secondaryPegOptions != null) {
-            cryptoOptions = secondaryPegOptions.toString();
-        }
-        return "NuBotOptions{" + "dualside=" + dualSide + ", submitLiquidity=" + submitLiquidity + ", executeOrders=" + executeOrders + ", verbose=" + verbose + ", sendHipchat=" + sendHipchat + ", apikey=" + apiKey + ", apisecret=" + apiSecret + ", nubitAddress=" + nubitAddress + ", rpcUser=" + rpcUser + ", rpcPass=" + rpcPass + ", nudIp=" + nudIp + ", nudPort=" + nudPort + ", priceIncrement=" + priceIncrement + ", txFee=" + txFee + ", exchangename=" + exchangeName + ", pair=" + pair + ", executeStrategyInterval=" + executeStrategyInterval + ", sendLiquidityInterval=" + sendLiquidityInterval + ", sendMails=" + sendMails + ", mailRecipient=" + mailRecipient + "emergencyTimeoutMinutes " + emergencyTimeout + "keepProceeds=" + keepProceeds + "aggregate=" + aggregate + " , waitBeforeShift=" + multipleCustodians + " , distributeLiquidity=" + distributeLiquidity + " , cryptoPegOptions=" + cryptoOptions + '}';
+        return "NuBotOptions{" + "dualside=" + dualSide + ", submitLiquidity=" + submitLiquidity + ", executeOrders=" + executeOrders + ", verbose=" + verbose + ", sendHipchat=" + sendHipchat + ", apikey=" + apiKey + ", apisecret=" + apiSecret + ", nubitAddress=" + nubitAddress + ", rpcUser=" + rpcUser + ", rpcPass=" + rpcPass + ", nudIp=" + nudIp + ", nudPort=" + nudPort + ", priceIncrement=" + priceIncrement + ", txFee=" + txFee + ", exchangename=" + exchangeName + ", pair=" + pair + ", executeStrategyInterval=" + executeStrategyInterval + ", sendLiquidityInterval=" + sendLiquidityInterval + ", sendMails=" + sendMails + ", mailRecipient=" + mailRecipient + "emergencyTimeoutMinutes " + emergencyTimeout + "keepProceeds=" + keepProceeds + "aggregate=" + aggregate + " , waitBeforeShift=" + multipleCustodians + " , distributeLiquidity=" + distributeLiquidity  + '}';
     }
 
     //Same as above, without printing api secret key and RCP password (for logging purposes)
@@ -716,10 +693,6 @@ public class NuBotOptions {
      * @return
      */
     public String toStringNoKeys() {
-        String cryptoOptions = "";
-        if (secondaryPegOptions != null) {
-            cryptoOptions = secondaryPegOptions.toHtmlString();
-        }
-        return "Options : {<br>" + "dualSide=" + dualSide + "<br> submitLiquidity=" + submitLiquidity + "<br> executeOrders=" + executeOrders + "<br> verbose=" + verbose + "<br> sendHipchat=" + sendHipchat + "<br> apiKey=" + apiKey + "<br> nubitAddress=" + nubitAddress + "<br> rpcUser=" + rpcUser + "<br> nudIp=" + nudIp + "<br> nudPort=" + nudPort + "<br> priceIncrement=" + priceIncrement + "<br> txFee=" + txFee + "<br> exchangename=" + exchangeName + "<br> pair=" + pair + "<br> executeStrategyInterval=" + executeStrategyInterval + "<br> sendLiquidityInterval=" + sendLiquidityInterval + "<br> sendMails=" + sendMails + "<br> mailRecipient=" + mailRecipient + "<br> emergencyTimeoutMinutes " + emergencyTimeout + "<br> keepProceeds=" + keepProceeds + "<br> aggregate=" + aggregate + "<br> distributeLiquidity=" + distributeLiquidity + " <br><br>" + cryptoOptions + '}';
+        return "Options : {<br>" + "dualSide=" + dualSide + "<br> submitLiquidity=" + submitLiquidity + "<br> executeOrders=" + executeOrders + "<br> verbose=" + verbose + "<br> sendHipchat=" + sendHipchat + "<br> apiKey=" + apiKey + "<br> nubitAddress=" + nubitAddress + "<br> rpcUser=" + rpcUser + "<br> nudIp=" + nudIp + "<br> nudPort=" + nudPort + "<br> priceIncrement=" + priceIncrement + "<br> txFee=" + txFee + "<br> exchangename=" + exchangeName + "<br> pair=" + pair + "<br> executeStrategyInterval=" + executeStrategyInterval + "<br> sendLiquidityInterval=" + sendLiquidityInterval + "<br> sendMails=" + sendMails + "<br> mailRecipient=" + mailRecipient + "<br> emergencyTimeoutMinutes " + emergencyTimeout + "<br> keepProceeds=" + keepProceeds + "<br> aggregate=" + aggregate + "<br> distributeLiquidity=" + distributeLiquidity  + '}';
     }
 }
