ee97c575834b70827a66c248ef395b1e5c0afc8f
benjyz <benjamin.l.cordes@gmail.com>
2015-03-03 21:44:11 +0100

    enter order test working. cancel test working

--- NuBot/src/com/nubits/nubot/tests/TestExchangePoloniex.java
+++ NuBot/src/com/nubits/nubot/tests/TestExchangePoloniex.java
@@ -16,6 +16,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class TestExchangePoloniex extends TestCase {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestExchangePoloniex.class
@@ -69,8 +72,6 @@ public class TestExchangePoloniex extends TestCase {
 
         Global.exchange = new Exchange(Global.options.getExchangeName());
 
-
-
         CurrencyPair testPair = Constant.NBT_BTC;
 
         WrapperTestUtils.configExchange(opt.getExchangeName());
@@ -105,6 +106,96 @@ public class TestExchangePoloniex extends TestCase {
         } else {
             assertTrue(false);
         }
+    }
+
+    @Test
+    public void testMakeOrder() {
+
+        NuBotOptions opt = null;
+        try {
+            opt = ParseOptions
+                    .parseOptionsSingle(testconfig);
+
+        } catch (NuBotConfigException e) {
+            e.printStackTrace();
+        }
+        Utils.loadProperties("settings.properties");
+
+        Global.options = opt;
+        Exchange exc = new Exchange(Global.options.getExchangeName());
+        assertTrue(exc != null);
+        Global.exchange =exc;
+        ExchangeLiveData liveData = new ExchangeLiveData();
+        Global.exchange.setLiveData(liveData);
+
+        ApiKeys keys = new ApiKeys(Global.options.getApiSecret(), Global.options.getApiKey());
+
+        Global.exchange = new Exchange(Global.options.getExchangeName());
+
+        CurrencyPair testPair = Constant.NBT_BTC;
+
+        WrapperTestUtils.configExchange(opt.getExchangeName());
+
+        TradeInterface ti = ExchangeFacade.getInterface(Global.exchange);
+        ti.setKeys(keys);
+        ti.setExchange(Global.exchange);
+        Global.exchange.setTrade(ti);
+        Currency btc = CurrencyList.BTC;
+
+        double tinyQty = 0.0000001;
+        double tinyPrice = 0.0000001;
+        ApiResponse orderresponse = ti.buy(testPair, tinyQty , tinyPrice);
+        //should raise  {"error":"Total must be at least 0.0001."}
+        assertTrue(!orderresponse.isPositive());
+
+        double minimialQty = 1;
+        double minimalPrice = 0.0001;
+        ApiResponse orderresponse2 = ti.buy(testPair, minimialQty, minimalPrice);
+        //should raise  {"error":"Total must be at least 0.0001."}
+        assertTrue(orderresponse2.isPositive());
+
+        if (orderresponse2.isPositive()) {
+            LOG.info("Positive response  from TradeInterface.getBalance() ");
+            Object o = orderresponse.getResponseObject();
+            LOG.info("response " + o);
+
+        } else {
+            assertTrue(false);
+        }
+
+        ApiResponse resp = ti.getActiveOrders();
+        assertTrue(resp.isPositive());
+
+        Object o = resp.getResponseObject();
+        ArrayList<Order> orderList = (ArrayList<Order>)o;
+
+        assertTrue(orderList.size()>0);
+
+        Order issued = orderList.get(0);
+
+        //cancel all
+        Iterator<Order> it = orderList.iterator();
+        while(it.hasNext()){
+            Order order = it.next();
+            ApiResponse cancelresp = ti.cancelOrder(order.getId(), testPair);
+            boolean success = ((Boolean)cancelresp.getResponseObject()).booleanValue();
+            assertTrue(success);
+        }
+
+        try{
+            Thread.sleep(1000);
+        }catch(Exception e){
+
+        }
+
+        ApiResponse resp2 = ti.getActiveOrders();
+        assertTrue(resp2.isPositive());
+
+        Object o2 = resp2.getResponseObject();
+        ArrayList<Order> orderList2 = (ArrayList<Order>)o2;
+
+        assertTrue(orderList2.size()==0);
+
     }
 }
 
