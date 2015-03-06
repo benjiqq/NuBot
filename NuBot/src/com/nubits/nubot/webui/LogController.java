ffc9864d45fc29a89eea874a6056acc9c8f91515
benjyz <benjamin.l.cordes@gmail.com>
2015-03-06 15:33:40 +0100

    UI: make log work

--- NuBot/src/com/nubits/nubot/webui/LogController.java
+++ NuBot/src/com/nubits/nubot/webui/LogController.java
@@ -1,5 +1,11 @@
 package com.nubits.nubot.webui;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.nubits.nubot.options.NuBotOptions;
+import com.nubits.nubot.options.NuBotOptionsSerializer;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -8,19 +14,26 @@ import static spark.Spark.get;
 
 public class LogController {
 
-    String filePath = "logs/NuBot_main.log";
+    /**
+     * the log file to send to client
+     */
+    String logfile = "logs/info.log";
 
     public LogController(String endpoint) {
 
         get(endpoint, "application/json", (request, response) -> {
+
+            JsonObject object = new JsonObject();
+
             try {
-                String l = new String(Files.readAllBytes(Paths.get(filePath)));
-                return l;
+                String l = new String(Files.readAllBytes(Paths.get(logfile)));
+                object.addProperty("log", l);
+                return object;
             }catch(Exception e){
 
             }
 
-            return "log";
+            return "error fetching log";
 
         });
 
