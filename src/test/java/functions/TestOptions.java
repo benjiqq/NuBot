a3744860229d319e5db4917261a5438e394cf302
benjyz <benjamin.l.cordes@gmail.com>
2015-03-09 15:25:21 +0100

    add back tests

---
+++ src/test/java/functions/TestOptions.java
@@ -0,0 +1,227 @@
+package functions;
+
+
+import com.nubits.nubot.options.NuBotConfigException;
+import com.nubits.nubot.options.NuBotOptions;
+import com.nubits.nubot.options.NuBotOptionsDefault;
+import com.nubits.nubot.options.ParseOptions;
+import com.nubits.nubot.utils.Utils;
+import junit.framework.TestCase;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestOptions extends TestCase {
+
+    private static String testconfigFile = "peatio.json";
+    private static String testconfig = "testconfig/" + testconfigFile;
+
+    @Override
+    public void setUp() {
+        try{
+            Utils.loadProperties("settings.properties");
+        }catch(IOException e){
+
+        }
+    }
+
+    @Test
+    public void testConfigExists() {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        final String wdir = System.getProperty("user.dir");
+        System.out.println("wdir: " + wdir);
+
+        File f = new File(testconfig);
+        System.out.println(">>> " + f.getAbsolutePath() + " exists : " + f.exists());
+        assertTrue(f.exists());
+    }
+
+    @Test
+    public void testLoadconfig() {
+        try {
+            JSONObject inputJSON = ParseOptions.parseSingleJsonFile(testconfig);
+            System.out.println(inputJSON);
+            assertTrue(inputJSON.keySet().size() > 0);
+        } catch (ParseException e) {
+            // e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testJson() {
+        try {
+            JSONObject inputJSON = ParseOptions.parseSingleJsonFile(testconfig);
+
+            assertTrue(inputJSON.containsKey("exchangename"));
+        } catch (ParseException e) {
+            // e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testLoadConfigJson() {
+        // used file
+        // {"options":
+        // {
+        // "dualside": true,
+        // "multiple-custodians":false,
+        // "submit-liquidity":true,
+        // "executeorders":false,
+        // "verbose":false,
+        // "hipchat":true,
+        // "mail-notifications":false,
+        // "mail-recipient":"xxx@xxx.xxx",
+        // "emergency-timeout":60,
+        // "keep-proceeds":0,
+        // "max-sell-order-volume" : 0,
+        // "max-buy-order-volume" : 0,
+        // "priceincrement": 0.1,
+        // }
+        // }
+
+        try {
+            // System.out.println(System.getProperty("));
+            // Global.options =
+            JSONObject j = ParseOptions
+                    .parseSingleJsonFile(testconfig);
+
+            assertTrue((boolean) j.get("verbose") == false);
+            assertTrue(((int) new Long((Long) j.get("emergencytimeout"))
+                    .intValue()) == 60);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testLoadConfig() {
+        try {
+            NuBotOptions nuo = ParseOptions
+                    .parseOptionsSingle(testconfig);
+
+            assertTrue(nuo != null);
+
+            assertTrue(nuo.exchangeName.equals("peatio"));
+
+            //assertTrue(nuo.getPair() != null);
+
+            //assertTrue(nuo.getSecondaryPegOptions() != null);
+            //.getSpread())
+
+        } catch (NuBotConfigException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testLoadComplete() {
+
+        String testconfigFile = "test.json";
+        String testconfig = "testconfig/" + testconfigFile;
+        boolean catched = false;
+
+        try {
+            JSONObject inputJSON = ParseOptions.parseSingleJsonFile(testconfig);
+            //assertTrue(inputJSON.containsKey("options"));
+            //JSONObject optionsJSON = ParseOptions.getOptionsKey(inputJSON);
+            assertTrue(inputJSON.containsKey("exchangename"));
+        } catch (Exception e) {
+
+        }
+        assertTrue(!catched);
+    }
+
+    @Test
+    public void testLoadOptions(){
+
+        String testconfigFile = "test.json";
+        String testconfig = "testconfig/" + testconfigFile;
+        boolean catched = false;
+        try {
+            NuBotOptions opt = ParseOptions.parseOptionsSingle(testconfig);
+
+            assertTrue(opt.isSubmitliquidity()==false);
+
+        } catch (NuBotConfigException e) {
+            System.out.println("could not parse config");
+            System.out.println(e);
+            catched = true;
+        }
+
+        assertTrue(!catched);
+
+
+    }
+
+    @Test
+    public void testLoadOptionsAll(){
+
+        String configFile = "config.json";
+        String configdir = "config";
+        String testconfig =configdir + "/" +  configFile;
+
+        boolean catched = false;
+        NuBotOptions opt = null;
+        try {
+             opt = ParseOptions.parseOptionsSingle(testconfig);
+
+        } catch (NuBotConfigException e) {
+            System.out.println("could not parse config");
+            System.out.println(e);
+            catched = true;
+        }
+
+        assertTrue(!catched);
+
+        /*String nudIp = NuBotOptionsDefault.nudIp;
+        String sendMails = NuBotOptionsDefault.sendMails;
+        boolean submitLiquidity = NuBotOptionsDefault.submitLiquidity;
+        boolean executeOrders = NuBotOptionsDefault.executeOrders;
+        boolean verbose = NuBotOptionsDefault.verbose;
+        boolean sendHipchat = NuBotOptionsDefault.sendHipchat;
+        boolean multipleCustodians = NuBotOptionsDefault.multipleCustodians;
+        int executeStrategyInterval = NuBotOptionsDefault.executeStrategyInterval;
+        double txFee = NuBotOptionsDefault.txFee;
+        double priceIncrement = NuBotOptionsDefault.priceIncrement;
+        double keepProceeds = NuBotOptionsDefault.keepProceeds;
+        double maxSellVolume = NuBotOptionsDefault.maxSellVolume;
+        double maxBuyVolume = NuBotOptionsDefault.maxBuyVolume;
+        int emergencyTimeout = NuBotOptionsDefault.emergencyTimeout;
+        boolean distributeLiquidity = NuBotOptionsDefault.distributeLiquidity;*/
+
+        assertTrue(opt.getPair()!=null);
+        //assertTrue(opt.getPair().equals("nbt_btc"));
+
+
+
+    }
+
+
+
+
+
+    // @Test
+    // public void testDefaultOptions() {
+    // // TODO
+    // assert (false);
+    // }
+    //
+    // @Test
+    // public void testcompulsory() {
+    // assert (false);
+    // }
+    //
+    // @Test
+    // public void testWrongConfig() {
+    // // TODO
+    // // if wrongly configured throws ParseError
+    // assert (false);
+    // }
+}
---
+++ src/test/java/testexchanges/TestExchangePoloniex.java
@@ -0,0 +1,187 @@
+package testexchanges;
+
+import com.nubits.nubot.bot.Global;
+import com.nubits.nubot.exchanges.ExchangeFacade;
+import com.nubits.nubot.global.Constant;
+import com.nubits.nubot.models.*;
+import com.nubits.nubot.options.NuBotConfigException;
+import com.nubits.nubot.options.NuBotOptions;
+import com.nubits.nubot.options.ParseOptions;
+import com.nubits.nubot.trading.TradeInterface;
+import com.nubits.nubot.utils.Utils;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * test Poloniex exchange
+ * WARNING: this uses live orders, with small amounts, but still
+ */
+public class TestExchangePoloniex extends TestCase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestExchangePoloniex.class
+            .getName());
+
+    private static String testconfigFile = "poloniex.json";
+    private static String testconfig = "testconfig/" + testconfigFile;
+
+    private TradeInterface ti;
+
+    @Test
+    public void testLoadConfig() {
+        System.out.println("test load config");
+
+        boolean catched = false;
+
+        NuBotOptions opt = null;
+        try {
+            opt = ParseOptions
+                    .parseOptionsSingle(testconfig);
+
+            assertTrue(opt != null);
+            assertTrue(opt.getExchangeName().equals("poloniex"));
+
+        } catch (NuBotConfigException e) {
+            catched = true;
+        }
+
+        assertTrue(!catched);
+    }
+
+
+    @Test
+    public void testGetBalance() {
+        System.out.println("get balance");
+        NuBotOptions opt = null;
+        try {
+            opt = ParseOptions
+                    .parseOptionsSingle(testconfig);
+            Global.options = opt;
+        } catch (NuBotConfigException e) {
+            e.printStackTrace();
+        }
+        try{
+            Utils.loadProperties("settings.properties");
+        }catch(IOException e){
+
+        }
+
+        ti = ExchangeFacade.exchangeInterfaceSetup(Global.options);
+
+        Currency btc = CurrencyList.BTC;
+        assertTrue(btc != null);
+
+        long start = System.currentTimeMillis();
+        ApiResponse balancesResponse = ti.getAvailableBalance(btc);
+        long stop = System.currentTimeMillis();
+        long delta = stop - start;
+        assertTrue(delta < 2000);
+
+        if (balancesResponse.isPositive()) {
+            LOG.info("Positive response  from TradeInterface.getBalance() ");
+            Object o = balancesResponse.getResponseObject();
+            LOG.info("response " + o);
+            try {
+                Amount a = (Amount) o;
+                assertTrue(a.getQuantity() >= 0);
+            } catch (Exception e) {
+                assertTrue(false);
+            }
+            //Balance balance = (Balance) o;
+
+            //LOG.info(balance.toStringSep());
+
+            //assertTrue(balance.getNubitsBalance().getQuantity() == 0.0);
+            //assertTrue(balance.getPEGBalance().getQuantity() == 1000.0);
+
+        } else {
+            assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testMakeOrder() {
+        NuBotOptions opt = null;
+        try {
+            opt = ParseOptions
+                    .parseOptionsSingle(testconfig);
+
+        } catch (NuBotConfigException e) {
+            e.printStackTrace();
+        }
+        try{
+            Utils.loadProperties("settings.properties");
+        }catch(IOException e){
+
+        }
+
+        ti = ExchangeFacade.exchangeInterfaceSetup(opt);
+
+        CurrencyPair testPair = Constant.NBT_BTC;
+
+        Currency btc = CurrencyList.BTC;
+
+        double tinyQty = 0.0000001;
+        double tinyPrice = 0.0000001;
+        ApiResponse orderresponse = ti.buy(testPair, tinyQty, tinyPrice);
+        //should raise  {"error":"Total must be at least 0.0001."}
+        assertTrue(!orderresponse.isPositive());
+
+        double minimialQty = 1;
+        double minimalPrice = 0.0001;
+        ApiResponse orderresponse2 = ti.buy(testPair, minimialQty, minimalPrice);
+        //should raise  {"error":"Total must be at least 0.0001."}
+        assertTrue(orderresponse2.isPositive());
+
+        if (orderresponse2.isPositive()) {
+            LOG.info("Positive response  from TradeInterface.getBalance() ");
+            Object o = orderresponse.getResponseObject();
+            LOG.info("response " + o);
+
+        } else {
+            assertTrue(false);
+        }
+
+        ApiResponse resp = ti.getActiveOrders();
+        assertTrue(resp.isPositive());
+
+        Object o = resp.getResponseObject();
+        ArrayList<Order> orderList = (ArrayList<Order>) o;
+
+        assertTrue(orderList.size() > 0);
+
+        Order issued = orderList.get(0);
+
+        //cancel all
+        Iterator<Order> it = orderList.iterator();
+        while (it.hasNext()) {
+            Order order = it.next();
+            ApiResponse cancelresp = ti.cancelOrder(order.getId(), testPair);
+            boolean success = ((Boolean) cancelresp.getResponseObject()).booleanValue();
+            assertTrue(success);
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+
+        }
+
+        //make sure there are no outstanding orders
+
+        ApiResponse resp2 = ti.getActiveOrders();
+        assertTrue(resp2.isPositive());
+
+        Object o2 = resp2.getResponseObject();
+        ArrayList<Order> orderList2 = (ArrayList<Order>) o2;
+
+        assertTrue(orderList2.size() == 0);
+
+    }
+}
+
