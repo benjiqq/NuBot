1811c9b6fbada8b6cb093c539a09f9385bd6c5a4
benjyz <benjamin.l.cordes@gmail.com>
2015-03-09 09:27:59 +0100

    mv folders

--- NuBot/src/com/nubits/nubot/trading/OrderException.java
+++
@@ -1,9 +0,0 @@
-package com.nubits.nubot.trading;
-
-
-public class OrderException extends Exception{
-
-    public OrderException(String msg){
-        super(msg);
-    }
-}
--- NuBot/src/main/java/com/nubits/nubot/tasks/strategy/FeedPriceException.java
+++
@@ -1,8 +0,0 @@
-package com.nubits.nubot.tasks.strategy;
-
-
-public class FeedPriceException extends Exception {
-    public FeedPriceException(String errorMessage) {
-        super(errorMessage);
-    }
-}
