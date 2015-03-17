f8ba9beedde5ecb832cc5a091d7ad6843ce0a4cb
Desrever Nu <desrever.nu@gmail.com>
2015-03-17 13:55:36 +0100

    added RPC test credentials

--- src/main/java/com/nubits/nubot/testsmanual/PasswordsTest.java
+++ src/main/java/com/nubits/nubot/testsmanual/PasswordsTest.java
@@ -3,8 +3,8 @@ package com.nubits.nubot.testsmanual;
 public class PasswordsTest {
 
    // RPC - Necessary for testing only
-    public static final String NUD_RPC_USER = "bla";
-    public static final String NUD_RPC_PASS = "yay";
-    public static final String CUSTODIAN_PUBLIC_ADDRESS = "xxx"; //testnet elected custodian
+    public static final String NUD_RPC_USER = "nubotTestUser";
+    public static final String NUD_RPC_PASS = "nubotTestPass";
+    public static final String CUSTODIAN_PUBLIC_ADDRESS = "bVcXrdTgrMSg6J2YqsLedCbi6Ubek9eTe5"; //testnet elected custodian
 
 }
