7a8fd88a9297df30bebf2e29232d568828edcaab
benjyz <benjamin.l.cordes@gmail.com>
2015-03-18 10:59:18 +0100

    add utility for testing classpaths

--- src/main/java/com/nubits/nubot/utils/ClasspathUtils.java
+++ src/main/java/com/nubits/nubot/utils/ClasspathUtils.java
@@ -1,25 +1,28 @@
 package com.nubits.nubot.utils;
 
+import sun.security.krb5.Config;
+
 import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.List;
 
 /**
  * a small utility to show the used classpath and VM arguments
- *
  */
-public class TestClasspathOut {
+public class ClasspathUtils {
 
     public static void main(String[] a) {
-        
+
         System.out.println("++++ system arguments...");
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
-        
+
         //-cp .:conf
-        
+
         RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
         List<String> arguments = runtimeMxBean.getInputArguments();
 
@@ -37,5 +40,24 @@ public class TestClasspathOut {
         for (int i = 0; i < classpathEntries.length; i++) {
             System.out.println(classpathEntries[i]);
         }
+
+
+        //see
+        // http://docs.oracle.com/javase/8/docs/api/java/lang/ClassLoader.html#getResources-java.lang.String-
+
+        ClassLoader classLoader = ClasspathUtils.class.getClassLoader();
+        // or use:
+        // ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        // depending on what's appropriate in your case.
+        try{
+            Enumeration<URL> roots = classLoader.getResources(".");
+            while (roots.hasMoreElements()) {
+                final URL url = roots.nextElement();
+                System.out.println(url);
+            }
+        }catch(Exception e){
+
+        }
+
     }
 }
