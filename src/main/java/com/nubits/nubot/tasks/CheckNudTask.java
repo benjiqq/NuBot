f87a82661017b51330913c1f121869235f634a57
benjyz <benjamin.l.cordes@gmail.com>
2015-03-19 11:44:27 +0100

    copyright notice with Template. don't use in xml or properties

--- src/main/java/com/nubits/nubot/tasks/CheckNudTask.java
+++ src/main/java/com/nubits/nubot/tasks/CheckNudTask.java
@@ -1,23 +1,7 @@
-/*
- * Copyright (C) 2014-2015 Nu Development Team
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+ackage com.nubits.nubot.tasks;
 
-/* 
- * Copyright (C) 2014-2015 Nu Development Team
+/*
+ * Copyright (C) 2015 Nu Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -33,7 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package com.nubits.nubot.tasks;
 
 import com.nubits.nubot.bot.Global;
 import org.slf4j.Logger;
@@ -41,6 +24,24 @@ import org.slf4j.LoggerFactory;
 
 import java.util.TimerTask;
 
+
+/*
+ * Copyright (C) 2015 Nu Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 public class CheckNudTask extends TimerTask {
 
