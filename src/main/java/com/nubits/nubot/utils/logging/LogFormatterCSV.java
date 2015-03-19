ffefbb46560275116ddb6c2059a67f46741a4803
benjyz <benjamin.l.cordes@gmail.com>
2015-03-19 12:46:08 +0100

    copyright notice before package

--- src/main/java/com/nubits/nubot/utils/logging/LogFormatterCSV.java
+++ src/main/java/com/nubits/nubot/utils/logging/LogFormatterCSV.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2014-2015 Nu Development Team
+ * Copyright (C) 2015 Nu Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
