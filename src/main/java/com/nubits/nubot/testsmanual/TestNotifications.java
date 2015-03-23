5a9e7c8d75b979ea63a77b36b21a1b9c1acb85a3
Desrever Nu <desrever.nu@gmail.com>
2015-03-23 11:27:25 +0100

    updated notification test

--- src/main/java/com/nubits/nubot/testsmanual/TestNotifications.java
+++ src/main/java/com/nubits/nubot/testsmanual/TestNotifications.java
@@ -19,17 +19,41 @@
 package com.nubits.nubot.testsmanual;
 
 
+import com.nubits.nubot.bot.Global;
 import com.nubits.nubot.notifications.HipChatNotifications;
 import com.nubits.nubot.notifications.MailNotifications;
+import com.nubits.nubot.options.NuBotConfigException;
+import com.nubits.nubot.options.ParseOptions;
+import com.nubits.nubot.utils.Utils;
 import io.evanwong.oss.hipchat.v2.rooms.MessageColor;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+
 public class TestNotifications {
+
+
+    private static final String TEST_OPTIONS_PATH = "config/myconfig/poloniex.json";
+
+    static {
+        System.setProperty("logback.configurationFile", "allconfig/testlog.xml");
+    }
+
 
     private static final Logger LOG = LoggerFactory.getLogger(TestNotifications.class.getName());
 
     public static void main(String[] a) {
+
+        //Load config
+        try {
+            Global.options = ParseOptions.parseOptionsSingle(TEST_OPTIONS_PATH);
+            LOG.info("using key: " + Global.options.getApiKey());
+            LOG.info("config exchange " + Global.options.getExchangeName());
+
+        } catch (NuBotConfigException ex) {
+            LOG.error(ex.toString());
+        }
 
         String email = "desrever.nu@gmail.com";
         MailNotifications.send(email, "Test Title", "Test Message");
