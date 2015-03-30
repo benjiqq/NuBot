72dd03f432cc4e602af9cf8f655657a25674ca3a
Desrever Nu <desrever.nu@gmail.com>
2015-03-30 22:55:00 +0200

    no message

--- src/main/java/com/nubits/nubot/global/PasswordsSample.java
+++ src/main/java/com/nubits/nubot/global/PasswordsSample.java
@@ -24,36 +24,6 @@ package com.nubits.nubot.global;
 public class PasswordsSample {
 
     /*
-     * RPC - Necessary for testing only
-     */
-    public static final String NUD_RPC_USER = "xxx";
-    public static final String NUD_RPC_PASS = "xxx";
-    public static final String CUSTODIAN_PUBLIC_ADDRESS = "xxx"; //testnet elected custodian
-     /*
-     * Exchanges - Necessary for testing only
-     */
-    public static final String BTCE_SECRET = "xxx";
-    public static final String BTCE_KEY = "xxx";
-    public static final String INTERNAL_PEATIO_SECRET = "xxx";
-    public static final String INTERNAL_PEATIO_KEY = "xxx";
-    public static final String CCEDK_SECRET = "xxx";
-    public static final String CCEDK_KEY = "xxx";
-    public static final String BTER_KEY = "xxx";
-    public static final String BTER_SECRET = "xxx";
-    public static final String POLONIEX_SECRET = "xxx";
-    public static final String POLONIEX_KEY = "xxx";
-    public static final String CCEX_SECRET = "xxx"; //
-    public static final String ALLCOIN_KEY = "xxx";
-    public static final String ALLCOIN_SECRET = "xxx";
-    public static final String BITSPARK_KEY = "xxx";
-    public static final String BITSPARK_SECRET = "xxx";
-    public static final String EXCOIN_KEY = "xxx";
-    public static final String EXCOIN_SECRET = "xxx";
-    public static final String BITCOINCOID_KEY = "xxx";
-    public static final String BITCOINCOID_SECRET = "xxx";
-    public static final String COMKORT_KEY = "xxx";
-    public static final String COMKORT_SECRET = "xxx";
-    /*
      * Misc - Necessary for build
      * ! Remember to add to build process in Jenkins !!
      */
