11a01686c00070c73e924d850fbc3b2797a038c4
benjyz <benjamin.l.cordes@gmail.com>
2015-04-17 11:00:51 +0200

    test_logback.xml

--- src/main/java/com/nubits/nubot/trading/wrappers/PoloniexWrapper.java
+++ src/main/java/com/nubits/nubot/trading/wrappers/PoloniexWrapper.java
@@ -754,7 +754,7 @@ public class PoloniexWrapper implements TradeInterface {
         private String createNonce() {
             //potential FIX: add some time to the nonce, since time sync has issues
             //long fixtime = 1000;
-            long toRet = System.currentTimeMillis(); // + fixtime;
+            long toRet = System.currentTimeMillis()*10000; // + fixtime;
             return Long.toString(toRet);
         }
     }
