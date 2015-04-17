8d3139673e2725b8093e6f36256c32dcb83ebd94
benjyz <benjamin.l.cordes@gmail.com>
2015-04-17 13:28:52 +0200

    clean class

--- src/main/java/com/nubits/nubot/utils/LogVerboseFilter.java
+++ src/main/java/com/nubits/nubot/utils/LogVerboseFilter.java
@@ -14,38 +14,24 @@ public class LogVerboseFilter extends ch.qos.logback.core.filter.AbstractMatcher
 
     @Override
     public FilterReply decide(Object event) {
-       /*if (!isStarted()) {
-            return FilterReply.NEUTRAL;
-        }*/
-
-        //System.out.println("event " + event);
 
         LoggingEvent loggingEvent = (LoggingEvent) event;
 
         if (loggingEvent.getLevel().equals(Level.WARN) || loggingEvent.getLevel().equals(Level.ERROR))
             return FilterReply.DENY;
 
+        //only filter if global.options is defined
         if (Global.options != null) {
             boolean isDebug = (loggingEvent.getLevel().equals(Level.DEBUG));
             if (Global.options.isVerbose() && isDebug) {
-                System.out.println("allow debug");
                 return FilterReply.NEUTRAL;
             } else {
-                System.out.println("deny debug");
                 return FilterReply.DENY;
             }
         } else {
             return FilterReply.NEUTRAL;
         }
 
-        /*List<Level> eventsToKeep = Arrays.asList(Level.TRACE, Level.DEBUG, Level.INFO);
-        if (Global.options!= null)
-            System.out.println("verbose: " + Global.options.isVerbose());
-        if (eventsToKeep.contains(loggingEvent.getLevel())) {
-            return FilterReply.NEUTRAL;
-        } else {
-            return FilterReply.DENY;
-        }*/
     }
 
 }
