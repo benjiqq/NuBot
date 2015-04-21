ac509da7d4146b76de6bf07bcdda9f6fe33e18c5
Desrever Nu <desrever.nu@gmail.com>
2015-04-21 15:47:56 +0200

    peatio

--- src/main/java/com/nubits/nubot/testsmanual/TestWrappers.java
+++ src/main/java/com/nubits/nubot/testsmanual/TestWrappers.java
@@ -43,7 +43,7 @@ public class TestWrappers {
     /**
      * Configure tests
      */
-    private static final String TEST_OPTIONS_PATH = "config/myconfig/allcoin.json";
+    private static final String TEST_OPTIONS_PATH = "config/myconfig/poloniex.json";
 
     public static final CurrencyPair testPair = CurrencyList.NBT_BTC;
     public static final Currency testCurrency = CurrencyList.NBT;
