339470dbdb1d2c006b11cfe7bd721453dc18b92e
Desrever Nu <desrever.nu@gmail.com>
2015-04-21 13:01:02 +0200

    reverted changed to avoid conflict before merging

--- res/UI/js/main.js
+++ res/UI/js/main.js
@@ -4,68 +4,51 @@
   var updateTime = 500;
   var debug = true;
 
-  var dotCounter = 1 ;
-  var botRunning = false;
+ function setRunning(){
+      $('#startbot').html("Running");
+      $('#startbot').removeClass("btn-primary");
+      $('#startbot').addClass("btn-success");
+      document.title = 'NuBot GUI - Running';
+      $("#favicon").attr("href","favicon.ico");
+ }
 
-  var logLine = 0;
-
-function toggleBot(running)
-{
-    if(running)
-    {
-      botRunning = true;
-      $('#togglebot').html("Stop Bot");
-      $('#togglebot').removeClass("btn-primary");
-      $('#togglebot').addClass("btn-success");
-
-      //$('#status-img').attr("src","img/bot_running.gif");
-
-      document.title = 'NuBot GUI - Running'+animateDots();
-      //$("#favicon").attr("href","favicon-running.ico");
-    }
-    else
-    {
-      botRunning = false;
-
-      $('#togglebot').html("Start Bot");
-
-      $('#togglebot').removeClass("btn-success");
-      $('#togglebot').addClass("btn-primary");
-      $('#togglebot').html("Start");
-
-      //$('#status-img').attr("src","img/bot_running.png");
+ function setHalted(){
+      $('#startbot').removeClass("btn-success");
+      $('#startbot').addClass("btn-primary");
+      $('#startbot').html("Start");
 
       $("#ordertable").find("tr:gt(0)").remove();
       $("#balancetable").find("tr:gt(0)").remove();
       document.title = 'NuBot GUI - Stopped';
-      //$("#favicon").attr("href","favicon.ico");
-    }
-}
+      $("#favicon").attr("href","favicon-running.ico");
 
-function updateNavbar(page) {
-    $('#operation-nav').removeClass('active');
-    $('#config-nav').removeClass('active');
-    $('#docu-nav').removeClass('active');
-
-    switch (page) {
-        case "operation":
-            $('#operation-nav').addClass('active')
-            loadbot();
-            break;
-        case "config":
-            loadconfig();
-             $('#config-nav').addClass('active')
-            break;
-        case "docu":
-            $('#docu-nav').addClass('active')
-            break;
-        case "disclaimer":
-            $('#disclaimer-nav').addClass('active')
-        break;
-    }
  }
 
-function updateConfigFile(){
+ function updateNavbar(page) {
+     $('#operation-nav').removeClass('active');
+     $('#config-nav').removeClass('active');
+     $('#setup-nav').removeClass('active');
+     $('#about-nav').removeClass('active');
+
+     switch (page) {
+         case "operation":
+             $('#operation-nav').addClass('active')
+             loadbot();
+             break;
+         case "config":
+             loadconfig();
+              $('#config-nav').addClass('active')
+             break;
+         case "setup":
+             $('#setup').addClass('active')
+             break;
+         case "about":
+             $('#about').addClass('active')
+            break;
+     }
+  }
+
+ function updateConfigFile(){
          $.ajax({
           type: "GET",
           dataType: "json",
@@ -80,7 +63,7 @@ function updateConfigFile(){
         });
 }
 
-function updateOrders(){
+ function updateOrders(){
      $.ajax({
       type: "GET",
       dataType: "json",
@@ -115,7 +98,6 @@ function updateOrders(){
              $("#ordertable").find("tr:gt(0)").remove();
 
             var orders = data["orders"];
-            updateFavico(orders.length);
             for (var i = 0; i < orders.length; i++){
                 var order = orders[i];
                 var type = order["type"];
@@ -130,6 +112,7 @@ function updateOrders(){
     setTimeout(updateOrders , 2000);
  }
 
+
 function updateStatus(){
          $.ajax({
           type: "GET",
@@ -140,7 +123,7 @@ function updateStatus(){
 
         })
         .done(function( data ) {
-            //console.log("status data: " + JSON.parse(JSON.stringify(data)));
+            //console.log("status data: " + data);
 
             $('#botstatus').html(data["status"]);
 
@@ -149,7 +132,7 @@ function updateStatus(){
             $('#duration').html(data["duration"]);
 
             if (data["status"] == "running"){
-                toggleBot(true);
+                setRunning();
             }
 
         });
@@ -157,6 +140,7 @@ function updateStatus(){
         //re-call the function
         setTimeout(updateStatus, updateTime);
 }
+
 
 function flashButton(cbtn){
 
@@ -188,86 +172,84 @@ function flashButtonRed(cbtn){
 
 }
 
-function startBot(){
-    if(confirm("Are you sure you want to start the bot?")){
-        if (debug) console.log("calling start on server");
-
-        var jsondata = JSON.stringify({
-            "operation": "start"});
-
-        $.ajax(baseurl + "/startstop", {
-                data : jsondata,
-                dataType: "json",
-                type : 'POST',
-                success: function(data){
-                    console.log("starting bot ok " + data);
-                    var success = data["success"];
-
-                    if (success){
-                     //on success of post change the color of the button
-                      toggleBot(true);
-                    } else{
-                        flashButtonRed(cbtn);
-                        alert(data["error"]);
-                    }
-
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    alert("error posting to server " + textStatus + " " + errorThrown);
-                }
-           });
-         }
-}
-
-function stopBot()
-{
-   if (debug) console.log("calling stop on server");
-      if(confirm("Are you sure you want to stop the bot?"))
-      {
-         var jsondata = JSON.stringify({
-           "operation": "stop"});
-
-         $.ajax(baseurl + "/startstop", {
-               data : jsondata,
-               dataType: "json",
-               type : 'POST',
-               success: function(data){
-                  console.log("stopping bot " + data);
-                  var success = data["success"];
-                   var cbtn = $('#togglebot');
-                  if (success){
-                   //on success of post change the color of the button
-                    flashButton(cbtn);
-                    $('#duration').html("");
-                    toggleBot(false);
-                  } else{
-                      alert(data["error"]);
-                      flashButtonRed(cbtn);
-                  }
-               },
-               error: function(XMLHttpRequest, textStatus, errorThrown) {
-                   alert("error posting to server " + textStatus + " " + errorThrown);
-               }
-          });
-      }
-}
-
 $(document).ready(function() {
 
     updateConfigFile();
     updateOrders();
 
 
-    $('#togglebot').click(function()
-      {
-        if(botRunning)
-            stopBot();
-        else
-            startBot();
-     });
-}); //end document.ready function
+  $('#startbot').click(function(){
+    if(confirm("Are you sure?")){
+        if (debug) console.log("calling start on server");
 
-function autoScroll(){
+        var jsondata = JSON.stringify({
+            "operation": "start"});
+
+      $.ajax(baseurl + "/startstop", {
+            data : jsondata,
+            dataType: "json",
+            type : 'POST',
+            success: function(data){
+                console.log("starting bot ok " + data);
+                var success = data["success"];
+
+
+                if (success){
+                 //on success of post change the color of the button
+                  setRunning();
+
+                } else{
+                    flashButtonRed(cbtn);
+                    alert(data["error"]);
+                }
+
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("error posting to server " + textStatus + " " + errorThrown);
+            }
+       });
+     }
+    });
+
+
+
+
+  $('#stopbot').click(function(){
+
+        if (debug) console.log("calling stop on server");
+
+        var jsondata = JSON.stringify({
+            "operation": "stop"});
+
+      $.ajax(baseurl + "/startstop", {
+            data : jsondata,
+            dataType: "json",
+            type : 'POST',
+            success: function(data){
+               console.log("stopping bot " + data);
+               var success = data["success"];
+                var cbtn = $('#stopbot');
+               if (success){
+                //on success of post change the color of the button
+                 flashButton(cbtn);
+                 $('#duration').html("");
+                 setHalted();
+               } else{
+                   alert(data["error"]);
+                   flashButtonRed(cbtn);
+               }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("error posting to server " + textStatus + " " + errorThrown);
+            }
+       });
+
+
+    });
+});
+
+
+    function autoScroll(){
 
         //autoscrolling. not used - how to make log readable also?
         var psconsole = $('#logarea');
@@ -276,38 +258,42 @@ function autoScroll(){
 
     }
 
-function updateLog() {
+    var i = 0;
 
-    var url = baseurl + "/logdump";
+    function updateLog() {
 
-    console.log("url  " + url);
-    $.ajax({
-      type: "GET",
-      dataType: "json",
-      url: url,
-    })
-    .fail(function() {
-      console.log("error loading log");
-    })
-    .done(function( data ) {
-      $('#logarea').val(data["log"]);
-    });
+        var url = baseurl + "/logdump";
 
-    logLine++;
+        console.log("url  " + url);
+        $.ajax({
+          type: "GET",
+          dataType: "json",
+          url: url,
+        })
+        .fail(function() {
+          console.log("error loading log");
+        })
+        .done(function( data ) {
+          $('#logarea').val(data["log"]);
+        });
 
-    if (logLine % 20 == 0) {
-        autoScroll();
+        i++;
+
+        if (i % 20 == 0) {
+            autoScroll();
+        }
+
+        setTimeout(updateLog , updateTime);
     }
 
-    setTimeout(updateLog , updateTime);
-}
 
-function loadbot() {
+    function loadbot() {
         updateLog();
         updateStatus();
     }
 
-function loadconfig() {
+
+    function loadconfig() {
 
         var url = "http://" + host + ":" + port + "/config";
 
@@ -347,9 +333,10 @@ function loadconfig() {
             $("#backupfeeds").attr("value", data["backupFeeds"]);
 
          });
+
     }
 
-function postall(){
+    function postall(){
 
         var posturl = "http://" + host + ":" + port + "/config";
 
@@ -421,7 +408,8 @@ function postall(){
         makePost(posturl, jsondata);
     }
 
-function makePost(url, jsondata){
+
+    function makePost(url, jsondata){
         //make HTTP post to server
 
         $.ajax(url, {
@@ -457,7 +445,7 @@ function makePost(url, jsondata){
        });
     }
 
-function postreset(){
+ function postreset(){
     if(confirm("Are you sure?")){
     var posturl = "http://" + host + ":" + port + "/configreset";
 
@@ -484,6 +472,7 @@ function postreset(){
           //reset values from server
           updateConfigFile();
           loadconfig();
+
         },
 
         error: function(xhr, textStatus, errorThrown) {
@@ -491,33 +480,5 @@ function postreset(){
             alert(xhr.responseText);
         }
    });
- }
+ Ã¥}
 }
-
-function updateIframe(pageName)
-{
-         $('#docu-iframe').attr('src', 'docs/'+pageName+'.html');
-}
-
-function updateFavico(howmany)
-{
-    var favicon = new Favico({
-        animation : 'popFade'
-    });
-
-    //intial value
-    favicon.badge(howmany);
- }
-
- function animateDots()
- {
-     toReturn = "";
-     dotCounter++;
-     if(dotCounter>4)
-     {
-         dotCounter=1;
-     }
-     for(i=0; i<dotCounter; i++)
-         toReturn+='.';
-     return toReturn;
- }
