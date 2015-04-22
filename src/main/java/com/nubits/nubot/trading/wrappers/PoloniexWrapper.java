c30d34c4bc6de649d3ddd91ed6682baf9bd737ec
benjyz <benjamin.l.cordes@gmail.com>
2015-04-22 17:49:57 +0200

    re 588: log error only after fail

--- src/main/java/com/nubits/nubot/trading/wrappers/PoloniexWrapper.java
+++ src/main/java/com/nubits/nubot/trading/wrappers/PoloniexWrapper.java
@@ -107,7 +107,7 @@ public class PoloniexWrapper implements TradeInterface {
                     long greaterNonce = new Long(subs);
                     int addNonce = 5;
                     this.nonceCount = greaterNonce + addNonce;
-                    LOG.debug("nonce error. retry with corrected nonce " + this.nonceCount);
+                    LOG.debug("retry with corrected nonce " + this.nonceCount);
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
@@ -144,7 +144,7 @@ public class PoloniexWrapper implements TradeInterface {
                 String errorMessage = (String) httpAnswerJson.get("error");
                 ApiError apiErr = errors.apiReturnError;
                 apiErr.setDescription(errorMessage);
-                LOG.error("Poloniex API returned an error: " + errorMessage);
+                LOG.debug("Poloniex API returned an error: " + errorMessage);
 
                 apiResponse.setError(apiErr);
             } else {
