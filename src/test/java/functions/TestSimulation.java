82301df45ceb243424da1453f5cf924156eb47ab
benjyz <benjamin.l.cordes@gmail.com>
2015-04-25 18:30:04 +0200

    add auto-test in simulation

---
+++ src/test/java/functions/TestSimulation.java
@@ -0,0 +1,42 @@
+package functions;
+/*
+ * Copyright (C) 2015 Nu Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+import com.nubits.nubot.models.CurrencyList;
+import com.nubits.nubot.models.CurrencyPair;
+import com.nubits.nubot.models.LastPrice;
+import com.nubits.nubot.pricefeeds.feedservices.BitfinexPriceFeed;
+import com.nubits.nubot.pricefeeds.feedservices.BlockchainPriceFeed;
+import com.nubits.nubot.pricefeeds.feedservices.BtcePriceFeed;
+import com.nubits.nubot.pricefeeds.feedservices.CoinbasePriceFeed;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * test NuBot in simulation
+ */
+public class TestSimulation extends TestCase {
+
+    @Test
+    public void testSimple() {
+
+
+    }
+
+
+}
